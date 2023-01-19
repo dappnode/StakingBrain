63ac5728045f0ff8fffdc6e1b5a9f90c94248ced
Pablo Mendez <mendez4a@gmail.com>
2023-01-19 11:53:14 +0100

    serve ui through backend

--- packages/brain/src/index.ts
+++ packages/brain/src/index.ts
@@ -1,11 +1,20 @@
-import { createServer } from "http";
-import { join } from "path";
-import { readFileSync } from "fs";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = createServer((req, res) => {
-  if (req.url === "/") {
-    const path = join(__dirname, "dist", req.url);
-    const content = readFileSync(path);
-    res.end(content);
-  }
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("*", (req, res) => {
+  console.log("request received");
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(80, () => {
+  console.log("server started on port 80");
 });
