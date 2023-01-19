15267969ad144afdaa2483644f05999407f8e18d
Pablo Mendez <mendez4a@gmail.com>
2023-01-19 12:15:19 +0100

    fix serve ui

--- packages/brain/src/index.ts
+++ packages/brain/src/index.ts
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.resolve(__dirname, "build")));
+
 app.get("*", (req, res) => {
   console.log("request received");
   res.sendFile(path.join(__dirname, "build", "index.html"));
