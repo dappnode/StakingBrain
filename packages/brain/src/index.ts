a91070b1c028dc0d8d9efc63c70bd13ead912ef3
Pablo Mendez <mendez4a@gmail.com>
2023-01-19 18:45:56 +0100

    add socket io to backend

--- packages/brain/src/index.ts
+++ packages/brain/src/index.ts
@@ -2,15 +2,32 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-import { Tag } from "@stakingbrain/common";
-
-const a: Tag = "obol";
+import { Server } from "socket.io";
+import http from "http";
 
 const mode = process.env.NODE_ENV || "development";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 console.log(`Running app in mode: ${mode}`);
 
 const app = express();
+const server = new http.Server(app);
+
+const io = new Server(server, { serveClient: false });
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected`, socket.id);
+
+  // JSON RPC over WebSockets
+  socket.on(
+    "rpc",
+    (rpcPayload: RpcPayload, callback: (res: RpcResponse) => void) => {
+      if (typeof callback !== "function")
+        return console.error("JSON RPC over WS req without cb", rpcPayload);
+
+      callback({ result: rpcPayload });
+    }
+  );
+});
 
 app.use(cors());
 app.use(express.json());
@@ -23,3 +40,23 @@ app.get("*", (req, res) => {
 app.listen(80, () => {
   console.log("server started on port 80");
 });
+
+// Utils
+
+// Types
+// TODO: check move to common
+interface RpcPayload {
+  method: string;
+  params: Args;
+}
+
+type Args = any[];
+
+export interface RpcResponse<R = any> {
+  result?: R;
+  error?: {
+    code: number;
+    message: string;
+    data?: any;
+  };
+}
