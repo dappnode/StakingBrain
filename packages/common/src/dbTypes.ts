1d80b775261a4eebc28d2026242ccb8f7ceee9b2
Pablo Mendez <mendez4a@gmail.com>
2023-01-20 17:51:20 +0100

    setup db template

--- packages/common/src/dbTypes.ts
+++ packages/common/src/dbTypes.ts
@@ -1,3 +1,25 @@
+// TODO: add types for ethereum addresses so the JSON schemas validate them
+
+/**
+ * DbSlot represents the line in the database for a given public key:
+ * @param pubkey - the public key
+ * @param tag - the protocol of the public key
+ * @param feeRecipient - the address of the fee recipient. This is the fee recipient that must be persisted
+ * @param feeRecipientValidator - the address of the validator of the fee recipient. This is the fee recipient that holds the truth but not the truth to be persisted
+ * @param automaticImport - whether the public key was automatically imported
+ */
+export interface DbSlot {
+  [pubkey: string]: {
+    tag: Tag;
+    feeRecipient: string;
+    feeRecipientValidator: string;
+    automaticImport: boolean;
+  };
+}
+
+/**
+ * Tag describes the protocol of the public key imported
+ */
 export type Tag =
   | "obol"
   | "diva"
---
+++ packages/common/src/schemas/index.ts
@@ -0,0 +1,2 @@
+import { resolve } from "path";
+import * as TJS from "typescript-json-schema";
