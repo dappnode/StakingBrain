89769be5deec62c0a2599770358bb7c48bc441b9
Diego <dsimog01@estudiantes.unileon.es>
2023-01-30 13:30:26 +0100

    Delete getValidatorParams (unnecessary)

--- packages/brain/src/modules/apiClients/validatorApiClient/getValidatorParams.ts
+++
@@ -1,6 +0,0 @@
-//import { ConsensusClient, Network } from "@stakingbrain/common";
-
-export function getValidatorParams(/*consensusClient: ConsensusClient<Network>*/) {
-  //TODO
-  return "DAppNodePackage-validator.teku-prater.dnp.dappnode.eth";
-}
