f57e56dc8ff2f9b0e7d9704c59aff785e314666a
pablomendezroyo <mendez4a@gmail.com>
2023-02-01 11:08:13 +0100

    Start dev no webpack

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /app && yarn && yarn build:webpack
-cd /app/packages/ui/ && yarn start:webpack:dev &
-cd /app/packages/brain && yarn start:webpack:dev &
+cd /app && yarn && yarn build
+cd /app/packages/ui/ && yarn start:dev &
+cd /app/packages/brain && yarn start:dev &
 
 wait
