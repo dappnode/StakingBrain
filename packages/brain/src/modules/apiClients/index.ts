c71939eba74708f422c0f8b27d05ad84a9ab820b
Diego <dsimog01@estudiantes.unileon.es>
2023-02-01 11:39:27 +0100

    Modified standard API client to allow HTTPS

--- packages/brain/src/modules/apiClients/index.ts
+++ packages/brain/src/modules/apiClients/index.ts
@@ -1,42 +1,90 @@
-import { ApiParams } from "@stakingbrain/common";
+import { readFileSync } from "fs";
+import https from "node:https";
+import http from "node:http";
+
+import { ApiParams, AllowedMethods } from "@stakingbrain/common";
 
 export class StandardApiClient {
-  baseUrl: string;
-  host: string;
-  authToken?: string;
-  keymanagerEndpoint?: string;
+  requestOptions: https.RequestOptions;
 
   constructor(apiParams: ApiParams) {
-    this.authToken = apiParams.authToken;
-    this.baseUrl = apiParams.baseUrl;
-    this.host = apiParams.host;
-    this.keymanagerEndpoint = apiParams.apiPath;
+    const urlOptions = new URL(apiParams.baseUrl + (apiParams.apiPath || ""));
+
+    this.requestOptions = {
+      hostname: urlOptions.hostname,
+      port: urlOptions.port,
+      path: urlOptions.pathname,
+      protocol: urlOptions.protocol,
+    };
+
+    this.requestOptions.headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + apiParams.authToken,
+    };
+
+    //Check if both cert path and password are provided
+    if (apiParams.certFile?.path && apiParams.certFile?.password) {
+      try {
+        this.requestOptions.pfx = readFileSync(apiParams.certFile.path);
+      } catch (e) {
+        console.log("Error while reading certificate file: " + e);
+        throw e;
+      }
+      this.requestOptions.passphrase = apiParams.certFile.password;
+    }
   }
 
   protected async request(
-    method: string,
-    url: string,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    method: AllowedMethods,
+    endpoint: string,
+    tls = false,
     body?: any
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ): Promise<any> {
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Authorization: "",
-      Host: this.host,
-    };
+    let req: http.ClientRequest;
 
-    if (this.authToken) {
-      headers.Authorization = `Bearer ${this.authToken}`;
+    this.requestOptions.method = method;
+    this.requestOptions.path =
+      this.requestOptions.path && this.requestOptions.path !== "/"
+        ? this.requestOptions.path + endpoint
+        : endpoint;
+
+    if (tls) {
+      //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+      this.requestOptions.rejectUnauthorized = false;
+
+      req = https.request(this.requestOptions);
+    } else {
+      req = http.request(this.requestOptions);
     }
 
-    const response = await fetch(url, {
-      method,
-      headers,
-      body: body ? body : undefined,
+    if (body) {
+      req.write(body);
+    }
+
+    req.on("error", (e) => {
+      console.error(e);
     });
-    if (response.ok) return await response.json();
-    throw new Error(response.statusText);
+
+    req.end();
+
+    return new Promise((resolve, reject) => {
+      req.on("response", (res) => {
+        let data = "";
+
+        res.on("data", (chunk) => {
+          data = chunk;
+        });
+
+        res.on("end", () => {
+          try {
+            resolve(JSON.parse(data));
+          } catch (e) {
+            console.log("Error while parsing response:" + e);
+            reject("Error while parsing response:" + e);
+          }
+        });
+      });
+    });
   }
 }
