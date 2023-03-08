a16652a9d255d4402e3fc1695c40ed616ad3e5c2
dsimog01 <dsimog01@estudiantes.unileon.es>
2023-03-08 11:51:12 +0000

    Non editable stakehouse tag

--- packages/brain/src/modules/stakingProtocols/stakehouse/index.ts
+++ packages/brain/src/modules/stakingProtocols/stakehouse/index.ts
@@ -1,4 +1,13 @@
+//import { Wizard } from "@blockswaplab/lsd-wizard/config.mjs";
+//import { Network } from "@stakingbrain/common";
+
 export class StakeHouseSDK {
+  /*wizard: Wizard; 
+
+  constructor(network: Omit<Network, "gnosis">) {
+    const provider = 
+  }*/
+
   async getLsdFeeRecipient(pubkey: string): Promise<string> {
     //TODO
 
