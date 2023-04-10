fe02960a81b84a850438b89ef3e4b9cca7cebccd
dsimog01 <dsimog01@estudiantes.unileon.es>
2023-04-10 14:02:37 +0000

    Get LSD fee recipient implemented

--- packages/brain/src/modules/stakingProtocols/stakehouse/index.ts
+++ packages/brain/src/modules/stakingProtocols/stakehouse/index.ts
@@ -1,18 +1,25 @@
-//import { Wizard } from "@blockswaplab/lsd-wizard/config.mjs";
-//import { Network } from "@stakingbrain/common";
+import { Wizard } from "@blockswaplab/lsd-wizard";
+import { executionClientUrl } from "../../../index.js";
+import { getDefaultProvider } from "ethers";
+import { ValidatorDetails } from "./types.js";
 
 export class StakeHouseSDK {
-  /*wizard: Wizard; 
+  wizard: Wizard;
 
-  constructor(network: Omit<Network, "gnosis">) {
-    const provider = 
-  }*/
+  constructor() {
+    const provider = getDefaultProvider(executionClientUrl);
+    this.wizard = new Wizard({ signerOrProvider: provider });
+  }
 
   async getLsdFeeRecipient(pubkey: string): Promise<string> {
-    //TODO
+    const validatorDetails: ValidatorDetails | undefined =
+      await this.wizard.helper.getValidatorDetails(pubkey);
 
-    throw new Error(
-      `Cannot get StakeHouse fee recipient for pubkey ${pubkey}: Not implemented`
-    );
+    if (!validatorDetails || !validatorDetails.feeRecipient)
+      throw new Error(
+        `Cannot get StakeHouse fee recipient for pubkey ${pubkey}: Not found. Your execution client might be out of sync.`
+      );
+
+    return validatorDetails.feeRecipient;
   }
 }
