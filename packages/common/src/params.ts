4fa6bb7715fd7d19d890cf348a721f5c16e6251c
dsimog01 <dsimog01@estudiantes.unileon.es>
2023-05-09 11:44:34 +0000

    Update MEV SP address for prater

--- packages/common/src/params.ts
+++ packages/common/src/params.ts
@@ -6,7 +6,8 @@ export const ROCKET_POOL_FEE_RECIPIENT =
   "0xd4E96eF8eee8678dBFf4d535E033Ed1a4F7605b7" as const;
 
 export const MEV_SP_ADDRESS_PRATER =
-  "0x0000000000000000000000000000000000000000" as const;
+  "0x553bd5a94bcc09ffab6550274d5db140a95ae9bc" as const;
 
+// TODO: Put the mainnet address here
 export const MEV_SP_ADDRESS_MAINNET =
   "0x0000000000000000000000000000000000000000" as const;
