bda37e573e92bddc083f1062d9865ea8bcb38586
pablomendezroyo <mendez4a@gmail.com>
2024-03-19 10:17:29 +0100

    remove unused

--- packages/brain/src/modules/apiClients/index.ts
+++ packages/brain/src/modules/apiClients/index.ts
@@ -1,6 +1,5 @@
 export { BeaconchaApi } from "./beaconcha.js";
 export { Beaconchain } from "./beaconchain.js";
-export { DappnodeSigningProover } from "./dappnodeSigningProover.js";
 export { ValidatorApi } from "./validator.js";
 export { StandardApi } from "./standard.js";
 export { Web3SignerApi } from "./web3signer.js";
