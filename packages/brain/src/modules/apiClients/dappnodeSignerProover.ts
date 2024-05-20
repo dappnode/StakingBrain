ecc273d7865cd35121cd323a37d48e47f5ced02b
pablomendezroyo <mendez4a@gmail.com>
2024-05-20 10:27:38 +0200

    remove log

--- packages/brain/src/modules/apiClients/dappnodeSignerProover.ts
+++ packages/brain/src/modules/apiClients/dappnodeSignerProover.ts
@@ -20,8 +20,6 @@ export class DappnodeSigningProover extends StandardApi {
   public async sendProofOfAttestation(
     proofOfAttestations: DappnodeSigningProoverPostRequest[]
   ): Promise<void> {
-    const endpoint = `${path.join(this.dappnodeSignEndpoint)}?network=${encodeURIComponent(this.network.toString())}`;
-    console.log(`Sending proof of attestation to ${endpoint}`);
     await this.request({
       method: "POST",
       endpoint: `${path.join(this.dappnodeSignEndpoint)}&network=${encodeURIComponent(this.network.toString())}`,
