0b84887cfb02e244b181db4dc5644d69fa1b5064
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-06-12 12:29:07 +0200

    Update domain to validators-proofs.dappnode.io/ (#312)

--- packages/brain/src/params.ts
+++ packages/brain/src/params.ts
@@ -8,6 +8,6 @@ export const params = {
   defaultTag: "solo" as Tag,
   uiPort: 80,
   launchpadPort: 3000,
-  defaultValidatorsMonitorUrl: "https://dappnode-signatures.io",
+  defaultValidatorsMonitorUrl: "https://validators-proofs.dappnode.io",
   defaultProofsOfValidationCron: 24 * 60 * 60 * 1000, // 1 day in ms
 };
