e3e8daeb646b8063c1ec606102d1b62ff10b9cd6
pablomendezroyo <mendez4a@gmail.com>
2024-09-04 11:50:28 +0200

    fix dev mode

--- entrypoint-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build must be located in entrypoint due to docker volume mount
-
-corepack enable && yarn set version berry
-
-cd /app && yarn && yarn build && yarn run dev &
-
-wait
