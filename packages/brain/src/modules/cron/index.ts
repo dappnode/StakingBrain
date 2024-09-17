7242c8957ecaeb74b7ee0df4db53cc4ed16215f8
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-09-17 15:44:05 +0200

    Start cron at the beggining pf the epoch (#331)
    * start cron at the beggining of the epoch
    
    * add comment

--- packages/brain/src/modules/cron/index.ts
+++ packages/brain/src/modules/cron/index.ts
@@ -1,4 +1,4 @@
 export { CronJob } from "./cron.js";
 export { reloadValidators } from "./reloadValidators.js";
 export { sendProofsOfValidation } from "./sendProofsOfValidation.js";
-export { trackValidatorsPerformance } from "./trackValidatorsPerformance.js";
+export { trackValidatorsPerformance, getSecondsToNextEpoch } from "./trackValidatorsPerformance.js";
