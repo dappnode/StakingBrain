60f6ebc940e56d60f9a3955dc5e721c7fb339f8d
pablomendezroyo <mendez4a@gmail.com>
2024-09-17 10:33:44 +0200

    remove sync comitee rewards and block proposal

--- packages/brain/src/modules/cron/trackValidatorsPerformance.ts
+++ packages/brain/src/modules/cron/trackValidatorsPerformance.ts
@@ -5,6 +5,14 @@ import { ValidatorStatus } from "../apiClients/types.js";
 import { BlockProposalStatus } from "../apiClients/postgres/types.js";
 import { BrainDataBase } from "../db/index.js";
 
+const logPrefix = "[CRON - trackValidatorsPerformance]: ";
+
+// TODO: at this moment Lighthouse client does not support retrieving:
+// - liveness of validator from finalized epoch:
+// ```400: BAD_REQUEST: request epoch 79833 is more than one epoch from the current epoch 79835```
+// - sync committee rewards:
+// ```404: NOT_FOUND: Parent state is not available! MissingBeaconState(0xa9592014ad4aa3d5dcc4ef67b669278a85fb4dbe80f12364f2486444b7db3927)```
+
 /**
  * Cron task that will track validators performance for the epoch finalized and store it in the Postgres DB.
  * If any issue is arisen during the process, it will be retried after 1 minute. If the issue persists until the epoch
@@ -34,106 +42,100 @@ export async function trackValidatorsPerformance({
   // skip if no validator indexes
   if (validatorPubkeys.length === 0) return;
 
-  let secondsToNextEpoch = getSecondsToNextEpoch({
-    currentUnixTime: Math.floor(Date.now() / 1000),
-    minGenesisTime,
-    secondsPerSlot
-  });
+  const epochFinalized = await beaconchainApi.getEpochHeader({ blockId: "finalized" });
+  let newEpochFinalized = epochFinalized;
 
-  let epochFinalized: number;
-  let lastEpochFinalized: number;
+  while (epochFinalized === newEpochFinalized) {
+    try {
+      logger.debug(`${logPrefix}Epoch finalized: ${epochFinalized}`);
 
-  try {
-    // get the finalized epoch
-    epochFinalized = await beaconchainApi.getEpochHeader({ blockId: "finalized" });
-    logger.debug(`Epoch finalized: ${epochFinalized}`);
-
-    // get validator indexes
-    // TODO: could be a better way to get the indexes? i.e store them in cache or brain DB
-    const validatorIndexes = await Promise.all(
-      validatorPubkeys.map(
-        async (pubkey) => (await beaconchainApi.getStateValidator({ state: "finalized", pubkey })).data.index
-      )
-    );
-
-    // get only active validators
-    const activeValidators = (
-      await beaconchainApi.postStateValidators({
-        body: {
-          ids: validatorIndexes,
-          statuses: validatorIndexes.map(() => ValidatorStatus.ACTIVE_ONGOING)
-        },
-        stateId: "finalized"
-      })
-    ).data.map((validator) => validator.index);
-
-    if (activeValidators.length === 0) {
-      logger.info("No active validators found");
-      return;
-    }
-
-    // check node health
-    const { el_offline, is_syncing } = (await beaconchainApi.getSyncingStatus()).data;
-    if (el_offline) {
-      throw new Error("EL Node is offline");
-    }
-    if (is_syncing) {
-      throw new Error("Node is syncing");
-    }
-
-    // get validators liveness
-    const validatorsLiveness = (
-      await beaconchainApi.getLiveness({ epoch: epochFinalized.toString(), validatorIndexes })
-    ).data;
-
-    // get block attestations
-    const validatorsAttestationsRewards = (
-      await beaconchainApi.getAttestationsRewards({
-        epoch: epochFinalized.toString(),
-        pubkeysOrIndexes: validatorIndexes
-      })
-    ).data.total_rewards;
-
-    // get sync committee rewards
-    const validatorsSyncCommitteeRewards = (
-      await beaconchainApi.getSyncCommitteeRewards({
-        blockId: "finalized",
-        validatorIndexesOrPubkeys: validatorIndexes
-      })
-    ).data;
-
-    // insert performance data
-    for (const validatorIndex of validatorIndexes) {
-      const liveness = validatorsLiveness.find((liveness) => liveness.index === validatorIndex)?.is_live;
-      const attestationsRewards = validatorsAttestationsRewards.find(
-        (liveness) => liveness.validator_index === validatorIndex
+      // get validator indexes
+      // TODO: could be a better way to get the indexes? i.e store them in cache or brain DB
+      logger.debug(`${logPrefix}Getting validator indexes from pubkeys`);
+      const validatorIndexes = await Promise.all(
+        validatorPubkeys.map(
+          async (pubkey) => (await beaconchainApi.getStateValidator({ state: "finalized", pubkey })).data.index
+        )
       );
-      const syncCommitteeRewardsStr = validatorsSyncCommitteeRewards.find(
-        (liveness) => liveness.validator_index === validatorIndex
-      )?.reward;
 
-      if (!liveness || !syncCommitteeRewardsStr || !attestationsRewards) {
-        logger.error(
-          `Missing data for validator ${validatorIndex}. live: ${liveness}, sync: ${syncCommitteeRewardsStr}, att: ${attestationsRewards}`
-        );
-        continue;
+      // get only active validators
+      logger.debug(`${logPrefix}$Getting active validators`);
+      const activeValidators = (
+        await beaconchainApi.postStateValidators({
+          body: {
+            ids: validatorIndexes,
+            statuses: validatorIndexes.map(() => ValidatorStatus.ACTIVE_ONGOING)
+          },
+          stateId: "finalized"
+        })
+      ).data.map((validator) => validator.index);
+
+      if (activeValidators.length === 0) {
+        logger.info(`${logPrefix}No active validators found`);
+        return;
       }
 
-      await postgresClient.insertPerformanceData({
-        validatorIndex: parseInt(validatorIndex),
-        epoch: epochFinalized,
-        slot: 1, // TODO: how to get the slot?
-        liveness,
-        blockProposalStatus: BlockProposalStatus.Missed, // TODO: how to get the block proposal status?
-        syncCommitteeRewards: parseInt(syncCommitteeRewardsStr),
-        attestationsRewards
-      });
+      // check node health
+      logger.debug(`${logPrefix}Checking node health`);
+      const { el_offline, is_syncing } = (await beaconchainApi.getSyncingStatus()).data;
+      if (el_offline) throw Error("EL Node is offline");
+      if (is_syncing) throw Error("Node is syncing");
 
+      // get block attestations
+      logger.debug(`${logPrefix}Getting attestations rewards for epoch ${epochFinalized}`);
+      const validatorsAttestationsRewards = (
+        await beaconchainApi.getAttestationsRewards({
+          epoch: epochFinalized.toString(),
+          pubkeysOrIndexes: validatorIndexes
+        })
+      ).data.total_rewards;
+
+      // insert performance data
+      for (const validatorIndex of validatorIndexes) {
+        //const liveness = validatorsLiveness.find((liveness) => liveness.index === validatorIndex)?.is_live;
+        const attestationsRewards = validatorsAttestationsRewards.find(
+          (liveness) => liveness.validator_index === validatorIndex
+        );
+
+        if (!attestationsRewards) {
+          logger.error(`${logPrefix}Missing data for validator ${validatorIndex}, att: ${attestationsRewards}`);
+          continue;
+        }
+
+        // write on db
+        logger.debug(`${logPrefix}Inserting performance data for validator ${validatorIndex}`);
+        await postgresClient.insertPerformanceData({
+          validatorIndex: parseInt(validatorIndex),
+          epoch: epochFinalized,
+          slot: 1, // TODO: how to get the slot?
+          blockProposalStatus: BlockProposalStatus.Missed, // TODO: how to get the block proposal status?
+          attestationsRewards
+        });
+      }
+
+      logger.debug(`${logPrefix}Performance data inserted for epoch ${epochFinalized}`);
       return;
+    } catch (error) {
+      logger.error(`${logPrefix}Error occurred: ${error}. Updating epoch finalized and retrying in 1 minute`);
+      // skip if the seconds to the next epoch is less than 1 minute
+      const minuteInSeconds = 60;
+      const currentUnixTime = Math.floor(Date.now() / 1000);
+      const secondsToNextEpoch = getSecondsToNextEpoch({ currentUnixTime, minGenesisTime, secondsPerSlot });
+      if (secondsToNextEpoch < minuteInSeconds) {
+        logger.warn(
+          `${logPrefix}Seconds to the next epoch is less than 1 minute (${secondsToNextEpoch}). Skipping until next epoch`
+        );
+        return;
+      }
+      // wait 1 minute without blocking the event loop
+      await new Promise((resolve) => setTimeout(resolve, minuteInSeconds * 1000));
+      // update epoch finalized
+      newEpochFinalized = await beaconchainApi.getEpochHeader({ blockId: "finalized" });
     }
-  } catch (error) {
-    logger.error(`Error occurred: ${error}`);
   }
+  logger.debug(
+    `${logPrefix}Epoch finalized changed: ${newEpochFinalized}, finished tracking performance for epoch ${epochFinalized}`
+  );
 }
 
 /**
