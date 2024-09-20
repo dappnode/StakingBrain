a510d5ec403024159a222ed2acbb61497bce6db9
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-09-20 19:52:49 +0200

    Fix postgres db initialization (#345)
    * fix postgres db initialization
    
    * fix queries

--- packages/brain/src/modules/apiClients/postgres/index.ts
+++ packages/brain/src/modules/apiClients/postgres/index.ts
@@ -1,8 +1,27 @@
 import postgres from "postgres";
 import logger from "../../logger/index.js";
 import { BlockProposalStatus, ValidatorPerformance } from "./types.js";
-import { PostgresApiError } from "./error.js";
 import { ConsensusClient, ExecutionClient } from "@stakingbrain/common";
+
+// Postgres has a built in class for errors PostgresError. i.e:
+// PostgresError: type "block_proposal_status" already exists
+//     at ErrorResponse (file:///app/node_modules/postgres/src/connection.js:788:26)
+//     at handle (file:///app/node_modules/postgres/src/connection.js:474:6)
+//     at Socket.data (file:///app/node_modules/postgres/src/connection.js:315:9)
+//     at Socket.emit (node:events:519:28)
+//     at addChunk (node:internal/streams/readable:559:12)
+//     at readableAddChunkPushByteMode (node:internal/streams/readable:510:3)
+//     at Readable.push (node:internal/streams/readable:390:5)
+//     at TCP.onStreamRead (node:internal/stream_base_commons:191:23) {
+//   severity_local: 'ERROR',
+//   severity: 'ERROR',
+//   code: '42710',
+//   where: `SQL statement "CREATE TYPE BLOCK_PROPOSAL_STATUS AS ENUM('Missed', 'Proposed', 'Unchosen')"\n` +
+//     'PL/pgSQL function inline_code_block line 5 at SQL statement',
+//   file: 'typecmds.c',
+//   line: '1170',
+//   routine: 'DefineEnum'
+// }
 
 enum Columns {
   validatorIndex = "validator_index",
@@ -19,6 +38,9 @@ enum Columns {
 
 export class PostgresClient {
   private readonly tableName = "validators_performance";
+  private readonly BLOCK_PROPOSAL_STATUS = "BLOCK_PROPOSAL_STATUS";
+  private readonly EXECUTION_CLIENT = "EXECUTION_CLIENT";
+  private readonly CONSENSUS_CLIENT = "CONSENSUS_CLIENT";
   private sql: postgres.Sql;
 
   /**
@@ -42,12 +64,9 @@ export class PostgresClient {
     const query = `
 SELECT pg_total_relation_size('${this.tableName}');
     `;
-    try {
-      const result = await this.sql.unsafe(query);
-      return result[0].pg_total_relation_size;
-    } catch (err) {
-      throw new PostgresApiError(`Error getting table size: ${err.message}`);
-    }
+
+    const result = await this.sql.unsafe(query);
+    return result[0].pg_total_relation_size;
   }
 
   /**
@@ -64,80 +83,67 @@ SELECT pg_total_relation_size('${this.tableName}');
    * The primary key will be a combination of validator_index and epoch.
    */
   public async initialize() {
+    // important: enum create types must be broken into separate conditional checks for each ENUM type before trying to create it.
+    // Check and create BLOCK_PROPOSAL_STATUS ENUM type if not exists
+    await this.sql.unsafe(`
+    DO $$
+    BEGIN
+      CREATE TYPE ${this.BLOCK_PROPOSAL_STATUS} AS ENUM('${BlockProposalStatus.Missed}', '${BlockProposalStatus.Proposed}', '${BlockProposalStatus.Unchosen}');
+    EXCEPTION
+        WHEN duplicate_object THEN NULL;
+    END $$;
+  `);
+
+    // Check and create EXECUTION_CLIENT ENUM type if not exists
+    await this.sql.unsafe(`
+    DO $$
+    BEGIN
+        CREATE TYPE ${this.EXECUTION_CLIENT} AS ENUM('${ExecutionClient.Besu}', '${ExecutionClient.Nethermind}', '${ExecutionClient.Geth}', '${ExecutionClient.Erigon}', '${ExecutionClient.Unknown}');
+    EXCEPTION
+        WHEN duplicate_object THEN NULL;
+    END $$;
+  `);
+
+    // Check and create CONSENSUS_CLIENT ENUM type if not exists
+    await this.sql.unsafe(`
+    DO $$
+    BEGIN
+        CREATE TYPE ${this.CONSENSUS_CLIENT} AS ENUM('${ConsensusClient.Teku}', '${ConsensusClient.Prysm}', '${ConsensusClient.Lighthouse}', '${ConsensusClient.Nimbus}', '${ConsensusClient.Unknown}');
+    EXCEPTION
+        WHEN duplicate_object THEN NULL;
+    END $$;
+  `);
+
     const query = `
-DO $$ 
-BEGIN
-  -- Check and create BLOCK_PROPOSAL_STATUS ENUM type if not exists
-  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'BLOCK_PROPOSAL_STATUS') THEN
-    CREATE TYPE BLOCK_PROPOSAL_STATUS AS ENUM('${BlockProposalStatus.Missed}', '${BlockProposalStatus.Proposed}', '${BlockProposalStatus.Unchosen}');
-  END IF;
-
-  -- Check and create EXECUTION_CLIENT ENUM type if not exists
-  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'EXECUTION_CLIENT') THEN
-    CREATE TYPE EXECUTION_CLIENT AS ENUM('${ExecutionClient.Besu}', '${ExecutionClient.Nethermind}', '${ExecutionClient.Geth}', '${ExecutionClient.Erigon}', '${ExecutionClient.Unknown}');
-  END IF;
-
-  -- Check and create CONSENSUS_CLIENT ENUM type if not exists
-  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'CONSENSUS_CLIENT') THEN
-    CREATE TYPE CONSENSUS_CLIENT AS ENUM('${ConsensusClient.Teku}', '${ConsensusClient.Prysm}', '${ConsensusClient.Lighthouse}', '${ConsensusClient.Nimbus}', '${ConsensusClient.Unknown}');
-  END IF;
-END $$;
-
 -- Create the table if not exists
 CREATE TABLE IF NOT EXISTS ${this.tableName} (
   ${Columns.validatorIndex} BIGINT NOT NULL,
   ${Columns.epoch} BIGINT NOT NULL,
-  ${Columns.executionClient} EXECUTION_CLIENT NOT NULL,
-  ${Columns.consensusClient} CONSENSUS_CLIENT NOT NULL,
+  ${Columns.executionClient} ${this.EXECUTION_CLIENT} NOT NULL,
+  ${Columns.consensusClient} ${this.CONSENSUS_CLIENT} NOT NULL,
   ${Columns.slot} BIGINT,
   ${Columns.liveness} BOOLEAN,
-  ${Columns.blockProposalStatus} BLOCK_PROPOSAL_STATUS,
+  ${Columns.blockProposalStatus} ${this.BLOCK_PROPOSAL_STATUS},
   ${Columns.syncCommitteeRewards} BIGINT,
   ${Columns.attestationsRewards} JSONB,
   ${Columns.error} TEXT,
   PRIMARY KEY (${Columns.validatorIndex}, ${Columns.epoch})
 );
 `;
-
-    try {
-      await this.sql.unsafe(query);
-      logger.info("Table created or already exists.");
-    } catch (err) {
-      //TODO: what to do if initialize fails?
-      logger.error("Error creating table:", err);
-    }
+    await this.sql.unsafe(query);
+    logger.info("Table created or already exists.");
   }
 
   /**
-   * Delete database table.
+   * Delete database table and its enum types.
    */
-  public async deleteDatabaseTable() {
-    const query = `
-    DROP TABLE IF EXISTS ${this.tableName};
-  `;
-    try {
-      await this.sql.unsafe(query);
-      logger.info("Table deleted.");
-    } catch (err) {
-      logger.error("Error deleting table:", err);
-    }
-  }
-
-  /**
-   * Delete enum types.
-   */
-  public async deleteEnumTypes(): Promise<void> {
-    const query = `
-    DROP TYPE IF EXISTS BLOCK_PROPOSAL_STATUS;
-    DROP TYPE IF EXISTS EXECUTION_CLIENT;
-    DROP TYPE IF EXISTS CONSENSUS_CLIENT;
-  `;
-    try {
-      await this.sql.unsafe(query);
-      logger.info("Enum types deleted.");
-    } catch (err) {
-      logger.error("Error deleting enum types:", err);
-    }
+  public async deleteDatabaseTableAndEnumTypes() {
+    await this.sql.unsafe(`DROP TABLE IF EXISTS ${this.tableName}`);
+    await this.sql.unsafe(`
+    DROP TYPE IF EXISTS ${this.BLOCK_PROPOSAL_STATUS};
+    DROP TYPE IF EXISTS ${this.EXECUTION_CLIENT};
+    DROP TYPE IF EXISTS ${this.CONSENSUS_CLIENT};
+  `);
   }
 
   /**
@@ -151,23 +157,19 @@ CREATE TABLE IF NOT EXISTS ${this.tableName} (
 INSERT INTO ${this.tableName} (${Columns.validatorIndex}, ${Columns.epoch}, ${Columns.slot}, ${Columns.liveness}, ${Columns.blockProposalStatus}, ${Columns.syncCommitteeRewards}, ${Columns.attestationsRewards}, ${Columns.error})
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     `;
-    try {
-      await this.sql.unsafe(query, [
-        data.validatorIndex,
-        data.epoch,
-        data.executionClient,
-        data.consensusClient,
-        data.slot ?? null,
-        data.liveness ?? null,
-        data.blockProposalStatus ?? null,
-        data.syncCommitteeRewards ?? null,
-        JSON.stringify(data.attestationsRewards) ?? null, // JSONB expects a string
-        data.error ?? null
-      ]);
-    } catch (err) {
-      logger.error("Error inserting data:", err);
-      // TODO: what to do if insert fails?
-    }
+
+    await this.sql.unsafe(query, [
+      data.validatorIndex,
+      data.epoch,
+      data.executionClient,
+      data.consensusClient,
+      data.slot ?? null,
+      data.liveness ?? null,
+      data.blockProposalStatus ?? null,
+      data.syncCommitteeRewards ?? null,
+      JSON.stringify(data.attestationsRewards) ?? null, // JSONB expects a string
+      data.error ?? null
+    ]);
   }
 
   /**
@@ -181,37 +183,29 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 SELECT * FROM ${this.tableName}
 WHERE ${Columns.validatorIndex} = ANY($1)
     `;
-    try {
-      const result = await this.sql.unsafe(query, [validatorIndexes]);
-      // TODO: add type for result
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return result.map((row: any) => ({
-        validatorIndex: row.validator_index,
-        epoch: row.epoch,
-        executionClient: row.execution_client,
-        consensusClient: row.consensus_client,
-        slot: row.slot,
-        liveness: row.liveness,
-        blockProposalStatus: row.block_proposal_status,
-        syncCommitteeRewards: row.sync_comittee_rewards,
-        attestationsRewards: row.attestations_rewards,
-        error: row.error
-      }));
-    } catch (err) {
-      logger.error("Error getting data:", err);
-      return [];
-    }
+
+    const result = await this.sql.unsafe(query, [validatorIndexes]);
+    // TODO: add type for result
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return result.map((row: any) => ({
+      validatorIndex: row.validator_index,
+      epoch: row.epoch,
+      executionClient: row.execution_client,
+      consensusClient: row.consensus_client,
+      slot: row.slot,
+      liveness: row.liveness,
+      blockProposalStatus: row.block_proposal_status,
+      syncCommitteeRewards: row.sync_comittee_rewards,
+      attestationsRewards: row.attestations_rewards,
+      error: row.error
+    }));
   }
 
   /**
    * Method to close the database connection.
    */
   public async close(): Promise<void> {
-    try {
-      await this.sql.end();
-      logger.info("Database connection closed.");
-    } catch (err) {
-      logger.error("Error closing the database connection:", err);
-    }
+    await this.sql.end();
+    logger.info("Database connection closed.");
   }
 }
