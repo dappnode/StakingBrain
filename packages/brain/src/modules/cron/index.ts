47e0ea0319a6a9aef1a2bd5c1e171eb9330b099a
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-09-25 22:22:02 +0200

    Reorg track validators performance cron (#355)
    * Reorg track validators performance cron
    
    * remove unused comment

--- packages/brain/src/modules/cron/index.ts
+++ packages/brain/src/modules/cron/index.ts
@@ -1,4 +1,7 @@
 export { CronJob } from "./cron.js";
 export { reloadValidators } from "./reloadValidators/index.js";
 export { sendProofsOfValidation } from "./sendProofsOfValidation/index.js";
-export { trackValidatorsPerformance } from "./trackValidatorsPerformance/index.js";
+export {
+  trackValidatorsPerformanceCron,
+  startWithinTenFirstPercentageOfEpoch
+} from "./trackValidatorsPerformance/index.js";
