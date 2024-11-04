2de656c9d34285f5f00b4747bc31fd124b37ff48
Mateu Miralles <52827122+mateumiralles@users.noreply.github.com>
2024-11-04 15:30:47 +0100

    Adding lido csm pkg dns for cors (#404)

--- packages/brain/src/modules/apiServers/launchpad/config.ts
+++ packages/brain/src/modules/apiServers/launchpad/config.ts
@@ -3,6 +3,8 @@ export const corsOptions = {
     "http://rocketpool-testnet.public.dappnode",
     "http://rocketpool.dappnode",
     "http://stader-testnet.dappnode",
-    "http://stader.dappnode"
+    "http://stader.dappnode",
+    "http://ui.lido-csm-holesky.dappnode",
+    "http://ui.lido-csm-mainnet.dappnode"
   ]
 };
