71503709e1d0ba995fda54026327916db281d6d6
pablomendezroyo <mendez4a@gmail.com>
2025-02-05 19:26:14 +0100

    Post FR always in validator client

--- packages/brain/src/modules/cron/reloadValidators/postValidatorsFeeRecipientsFromDb.ts
+++ packages/brain/src/modules/cron/reloadValidators/postValidatorsFeeRecipientsFromDb.ts
@@ -4,37 +4,22 @@ import logger from "../../logger/index.js";
 import { logPrefix } from "./logPrefix.js";
 
 /**
- * Post in the validator API fee recipients that are in the DB and not in the validator API
+ * Post in the validator API fee recipients that are in the DB in the validator API
  */
 export async function postValidatorsFeeRecipientsFromDb({
   validatorApi,
-  dbData,
-  validatorPubkeysFeeRecipients
+  dbData
 }: {
   validatorApi: ValidatorApi;
   dbData: StakingBrainDb;
-  validatorPubkeysFeeRecipients: { pubkey: string; feeRecipient: string }[];
 }): Promise<void> {
-  const feeRecipientsToPost = validatorPubkeysFeeRecipients
-    .filter(
-      (validator) =>
-        validator.feeRecipient.toLocaleLowerCase() !== dbData[validator.pubkey].feeRecipient.toLocaleLowerCase()
-    )
-    .map((validator) => ({
-      pubkey: validator.pubkey,
-      feeRecipient: dbData[validator.pubkey].feeRecipient
-    }));
-
-  if (feeRecipientsToPost.length > 0) {
-    logger.debug(`${logPrefix}Found ${feeRecipientsToPost.length} fee recipients to add/update to validator API`);
-    for (const { pubkey, feeRecipient } of feeRecipientsToPost)
-      await validatorApi
-        .setFeeRecipient(feeRecipient, pubkey)
-        .catch((e) =>
-          logger.error(
-            `${logPrefix}Error adding fee recipient ${feeRecipient} to validator API for pubkey ${pubkey}`,
-            e
-          )
-        );
+  for (const [pubkey, { feeRecipient }] of Object.entries(dbData)) {
+    await validatorApi
+      .setFeeRecipient(feeRecipient, pubkey)
+      .catch((e) =>
+        logger.error(
+          `${logPrefix}Error posting fee recipient ${feeRecipient} for pubkey ${pubkey} to validator API: ${e}`
+        )
+      );
   }
 }
