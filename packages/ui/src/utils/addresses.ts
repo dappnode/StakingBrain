ea6500ff32ac1e451acbd344d10e155b397b6594
Marketen <marcfont12@gmail.com>
2025-07-01 16:54:26 +0200

    fix network typo

--- packages/ui/src/utils/addresses.ts
+++ packages/ui/src/utils/addresses.ts
@@ -2,7 +2,7 @@ import { MEV_SP_ADDRESS_MAINNET, MEV_SP_ADDRESS_HOODI, Network } from "@stakingb
 
 // if not in a network that has a Smooth, return null
 export const getSmoothAddressByNetwork = (network: Network) => {
-  if (network == "prater") {
+  if (network == "hoodi") {
     return MEV_SP_ADDRESS_HOODI;
   } else if (network == "mainnet") {
     return MEV_SP_ADDRESS_MAINNET;
